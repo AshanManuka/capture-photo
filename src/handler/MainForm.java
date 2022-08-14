2feca3189dd517cce3f7ad2c7de7c840020f146d
Manuka <manukajayarathne.coma@gmail.com>
2022-08-15 00:00:54 +0530

    Initialize project & main method

---
+++ src/handler/MainForm.java
@@ -0,0 +1,13 @@
+package handler;
+
+import com.github.sarxos.webcam.Webcam;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainForm {
+    public static void main(String[] args) throws IOException {
+    }
+}
