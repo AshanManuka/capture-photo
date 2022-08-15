5a810eb2c8942245376e88c2714fd56ff6abec57
Manuka <manukajayarathne.coma@gmail.com>
2022-08-15 09:03:18 +0530

    make logic

--- src/handler/MainForm.java
+++ src/handler/MainForm.java
@@ -9,5 +9,17 @@ import java.util.Scanner;
 
 public class MainForm {
     public static void main(String[] args) throws IOException {
+        getPermission();
     }
+
+    private static void getPermission() {
+        System.out.print("Do you want to Open camera ?  ");
+        Scanner scanner = new Scanner(System.in);
+        String select = scanner.next();
+
+        if (select.equals("y")){
+           // openCamera();
+        }
+    }
+
 }
